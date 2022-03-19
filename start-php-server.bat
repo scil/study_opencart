bc11e3d7e218fedccda01e8e11b26577b4e368ab
scil <caoxie.zc@gmail.com>
2022-03-19 17:33:20 +0800

    .

--- start-php-server.bat
+++ start-php-server.bat
@@ -1,3 +1,6 @@
+echo url:       localhost:8000/admin
+echo name:  admin
+echo pw:      jinzhao
 :: router.php has not been tested seccusefully
 ::php -S localhost:8000 -t upload/ router.php
 php -S localhost:8000 -t upload/   -c upload\php.ini 
